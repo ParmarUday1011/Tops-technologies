c7d209fd55f5c85480f978d70e59f10533ba6356
Parmar uday <106581710+ParmarUday1011@users.noreply.github.com>
2025-12-17 10:49:17 +0530

    Add files via upload

---
+++ React JS/Assignment /Practice/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Assignment402 from './Component/Assignment402';
+// import Assignment401 from './Component/Assignment401';
+// import Assignment302 from './Component/Assignment302';
+// import Assignment301 from './Component/Assignment301';
+// import Assignment2 from './Component/Assignment2';
+// import Assignment1 from './Component/Assignment1';
+
+function App() {
+  return (
+   <>
+   {/* <Assignment1/> */}
+   {/* <Assignment2/> */}
+   {/* <Assignment301 name="parmar"/> */}
+   {/* <Assignment302/> */}
+   {/* <Assignment401 name="uday" age="22" location="rajkot"/> */}
+   <Assignment402/>
+   </>
+  );
+}
+
+export default App;
---
+++ React JS/Assignment /Practice/src/Component/Assignment401.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Assignment401({name,age,location}) {
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <h2>Age: {age}</h2>
+      <h2>Location: {location}</h2>
+    </div>
+  )
+}
