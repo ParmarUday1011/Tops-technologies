39b2fa64d3b1c49d081df017605393a404e9a256
Parmar uday <106581710+ParmarUday1011@users.noreply.github.com>
2026-01-24 16:25:08 +0530

    Add files via upload

---
+++ React JS/Assignment/Core/Practical/myapp/src/Component/Assignment302.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Assignment302 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to react !!</h1>
+      </div>
+    )
+  }
+}
